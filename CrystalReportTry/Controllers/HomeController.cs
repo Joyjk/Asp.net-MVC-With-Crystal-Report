2227e5ce0a51a7646e381da0824de74c1f612cc9
Joyjk <17-35526-3@student.aiub.edu>
2022-02-02 21:27:08 +0600

    Add project files.

---
+++ CrystalReportTry/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Bogus;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalReportTry.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalReportTry.Controllers
+{
+    public class HomeController : Controller
+    {
+        ReportContext reportContext = new ReportContext(); 
+        public ActionResult Index()
+        {
+
+            reportContext.EmployeeInfoes.RemoveRange(reportContext.EmployeeInfoes);
+            reportContext.SaveChanges();
+
+            var fakeData = new Faker<EmployeeInfo>()
+                .RuleFor(x => x.EmployeeName, f => f.Name.FullName())
+                .RuleFor(x=>x.Email,f=>f.Internet.Email())
+                .RuleFor(x=>x.Phone,f=>f.Person.Phone)
+                .RuleFor(x=>x.Experience,f=>f.Random.Number(1,15));
+
+            var addAll = fakeData.Generate(100);
+            reportContext.EmployeeInfoes.AddRange(addAll);
+            reportContext.SaveChanges();
+            return View(reportContext.EmployeeInfoes.ToList());
+        }
+
+        public ActionResult exportReport()
+        {
+            ReportDocument rd = new ReportDocument();
+
+            rd.Load(Path.Combine(Server.MapPath("~/Report"), "CrystalReport.rpt"));
+            rd.SetDataSource(reportContext.EmployeeInfoes.ToList());
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "Employee_list.pdf");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CrystalReportTry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrystalReportTry.MvcApplication" Language="C#" %>
